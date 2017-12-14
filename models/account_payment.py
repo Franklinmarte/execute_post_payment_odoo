78ff150881b97500653cc60248aee3ba9dea06f8
franklinmarte <franklinmarte@cloudnine.com.do>
2017-12-14 13:13:24 -0400

    detalle

--- models/account_payment.py
+++ models/account_payment.py
@@ -2,7 +2,7 @@ from odoo import api, fields, models
 
 
 class AccountPayment(models.Model):
-	_inherti = 'account.payment'
+	_inherit = 'account.payment'
 
 	def executePost(self, post_id):
 		payment_obj = self.browse(post_id)
