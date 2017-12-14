0360deeb1dd3dadce153fe64895be0be59a16607
franklinmarte <franklinmarte@cloudnine.com.do>
2017-12-14 13:54:43 -0400

    change

--- models/account_payment.py
+++ models/account_payment.py
@@ -1,9 +1,13 @@
 from odoo import api, fields, models 
+import logging
+_logger = logging.getLogger(__name__)
 
 
 class AccountPayment(models.Model):
 	_inherit = 'account.payment'
 
 	def executePost(self, post_id):
+		_logger.info("HEMOS ENTRADO\n")
 		payment_obj = self.browse(post_id)
-		return payment_obj.post()
+		_logger.info("VALUE: %s" % payment_obj)
+		return payment_obj.post() if payment_obj else {}
